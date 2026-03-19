2173877457dbf9f8d8ae2e09db4f8ee31d6400cf
GrandBIRDLizard <justin.munch9@gmail.com>
2026-03-19 00:55:17 -0400

    clean and update files

---
+++ i3/i3-screenlayout.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Let X/DP/VRR settle after login
+sleep 1
+
+xrandr \
+  --output DisplayPort-0 \
+    --primary \
+    --mode 2560x1440 \
+    --rate 120 \
+    --pos 1080x385 \
+    --rotate normal \
+  --output HDMI-A-0 \
+    --mode 1920x1080 \
+    --pos 0x0 \
+    --rotate left \
+  --output DisplayPort-1 --off \
+  --output DisplayPort-2 --off \
+  --output HDMI-A-1-1 --off \
+  --output DisplayPort-1-3 --off \
+  --output DisplayPort-1-4 --off \
+  --output DisplayPort-1-5 --off
